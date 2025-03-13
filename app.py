e64edb0d788faece034f0cd4e39bd241a4d1e16e
ThanhHai <134618801+Tran-Hai@users.noreply.github.com>
2025-03-13 08:50:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ if not group:
 else:
     df2 = df1[df1['Group'].isin(group)]
 
-st.write(df2)
 
 with col1:
     figure_ht = create_pie_chart(df2, 'HT', 'Hiện Tượng', ma_hien_tuong)
@@ -52,6 +51,12 @@ with col3:
     figure_nng = create_pie_chart(df2, 'NNG', 'Nguyên Nhân Gốc', ma_nguyen_nhan_goc)
     st.plotly_chart(figure_nng, use_container_width=True, key = 'pie_nng')
 
+
+line_df = df2['Line'].value_counts().reset_index()
+line_df.columns = ['Line', 'Count']
+
+fig = px.bar(line_df, x = 'Line', y = 'Count', title = 'Incident Frequency of Each Line')
+st.plotly_chart(fig, use_container_width=True)
 
 cl1, cl2 = st.columns(2)
 
