803fca3c76d0bdc31f6bdbf58740951e9371578e
ThanhHai <134618801+Tran-Hai@users.noreply.github.com>
2025-03-13 14:12:19 +0700

    Update assy_page.py

--- pages/assy_page.py
+++ pages/assy_page.py
@@ -19,14 +19,17 @@ st.header('Linh Kiện Đồng Bộ')
 col1, col2, col3 = st.columns(3)
 
 with col1:
+    new_ba_df = ba_df[['LKDB', 'Line']]
     figure_ba = create_bar_chart(ba_df, 'LKDB', ma_linh_kien, 'BA')
     st.plotly_chart(figure_ba, use_container_width=True, key = 'bar_ba')
 
 with col2:
+    new_be_df = be_df[['LKDB', 'Line']]
     figure_be = create_bar_chart(be_df, 'LKDB', ma_linh_kien, 'BE')
     st.plotly_chart(figure_be, use_container_width=True, key = 'bar_be')
 
 with col3:
+    new_bm_df = bm_df[['LKDB', 'Line']]
     figure_bm = create_bar_chart(bm_df, 'LKDB', ma_linh_kien, 'BM')
     st.plotly_chart(figure_bm, use_container_width=True, key = 'bar_bm')
 
@@ -40,13 +43,16 @@ m_df = df[df['LKKTTR'].str.startswith('M')]
 col4, col5, col6 = st.columns(3)
 
 with col4:
+    new_a_df = a_df[['LKKTTR', 'Line']]
     figure_a = create_bar_chart(a_df, 'LKKTTR', ma_linh_kien, 'BA')
     st.plotly_chart(figure_a, use_container_width=True, key = 'bar_a')
 
 with col5:
+    new_e_df = e_df[['LKKTTR', 'Line']]
     figure_e = create_bar_chart(e_df, 'LKKTTR', ma_linh_kien, 'BE')
     st.plotly_chart(figure_e, use_container_width=True, key = 'bar_e')
 
 with col6:
+    new_m_df = m_df[['LKKTTR', 'Line']]
     figure_m = create_bar_chart(m_df, 'LKKTTR', ma_linh_kien, 'BM')
     st.plotly_chart(figure_m, use_container_width=True, key = 'bar_m')
